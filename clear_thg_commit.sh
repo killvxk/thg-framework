88b657324e30dba4fb3070389fa0156bc73e6195
darkcode357 <Mortebranca357@357>
2019-08-01 23:41:38 -0300

    -a

--- clear_thg_commit.sh
+++
@@ -1,8 +0,0 @@
-echo "clear pyc"
-find . -name "*.pyc" -exec rm -f {} \;
-echo ".idea/*"
-find . -name "*.idea/*" -exec rm -f {} \;
-echo "___pycache__"
-find . -name "pycache" -exec rm -f {} \;
-
-python3 commit.py
