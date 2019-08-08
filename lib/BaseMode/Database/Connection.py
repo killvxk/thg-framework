43dd821b54d5fe2231ed60ecd82f4541d5d0a889
Luis Roseno <nando.roseno@gmail.com>
2019-08-08 00:50:04 -0300

    DB Connection fixed

---
+++ lib/BaseMode/Database/Connection.py
@@ -0,0 +1,18 @@
+from mongoengine import connect
+from dotenv import load_dotenv, find_dotenv, get_key
+
+dotenv_file = find_dotenv()
+load_dotenv(dotenv_file)
+
+db_name = get_key(dotenv_file, "MONGODB_DATABASE")
+db_user = get_key(dotenv_file, "MONGODB_USERNAME")
+db_pass = get_key(dotenv_file, "MONGODB_PASSWORD")
+
+def connect_db():
+    db = connect(
+        db=db_name,
+        username=db_user,
+        password=db_pass,
+        host='mongodb://'+db_user+':'+db_pass+'@localhost/'+db_name
+    )
+    return db
